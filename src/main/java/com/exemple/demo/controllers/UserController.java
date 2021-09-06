76b40a1731eb352ceb0a193d5fe4af1a285ac0e6
milinfic <paulo.cpa@hotmail.com>
2021-09-06 20:37:16 -0300

    segundo commit - Implementação da função page

--- src/main/java/com/exemple/demo/controllers/UserController.java
+++ src/main/java/com/exemple/demo/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.exemple.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.exemple.demo.entities.User;
 import com.exemple.demo.repositories.UserRepository;
 
-@RestController //para mostrar que vai ser um controlador rest
-@RequestMapping(value = "/user")
+@RestController 		//para mostrar que vai ser um controlador rest
+@RequestMapping(value = "/users")
 public class UserController {
 
 	//esse componente sera injetado automáticamente
 	@Autowired
 	private UserRepository repository;
 
+	
+	//Esse método irá buscar todos os dados do bd 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 	    List<User> result = repository.findAll();
 	    return ResponseEntity.ok(result);
 	}
+	
+	//esse método irá retornar page (quantidade de itens por página) do caminho /users/page
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<User>> findAll(Pageable pageable){
+		Page<User> result = repository.findAll(pageable);		
+		return ResponseEntity.ok(result);		
+	}	
+	
 	
 }
