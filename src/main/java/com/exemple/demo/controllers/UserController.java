e70e04814422568f8c23f6b1c3585459dd585c9c
milinfic <paulo.cpa@hotmail.com>
2021-09-06 21:45:23 -0300

    Terceiro commit - Finalização do projeto

--- src/main/java/com/exemple/demo/controllers/UserController.java
+++ src/main/java/com/exemple/demo/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemple.demo.entities.User;
@@ -19,8 +20,7 @@ public class UserController {
 
 	//esse componente sera injetado automáticamente
 	@Autowired
-	private UserRepository repository;
-
+	private UserRepository repository;	
 	
 	//Esse método irá buscar todos os dados do bd 
 	@GetMapping
@@ -35,6 +35,36 @@ public class UserController {
 		Page<User> result = repository.findAll(pageable);		
 		return ResponseEntity.ok(result);		
 	}	
+	
+	@GetMapping(value = "/search-salary")
+	public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+	    Page<User> result = repository.searchSalary(minSalary, maxSalary, pageable);
+	    return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search-name")
+	public ResponseEntity<Page<User>> searchByName(@RequestParam(defaultValue = "") String name, 
+			Pageable pageable) {
+	    Page<User> result = repository.searchByName(name, pageable);
+	    return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search-salary-method-2")
+	public ResponseEntity<Page<User>> findBySalaryBetween(
+			@RequestParam(defaultValue = "0") Double minSalary, 
+			@RequestParam(defaultValue = "1000000000000") Double maxSalary, 
+			Pageable pageable) {
+	    Page<User> result = repository.searchSalary(minSalary, maxSalary, pageable);
+	    return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search-name-method-2")
+	public ResponseEntity<Page<User>> findByNameContainingIgnoreCase(
+			@RequestParam(defaultValue = "") String name, 
+			Pageable pageable) {
+	    Page<User> result = repository.searchByName(name, pageable);
+	    return ResponseEntity.ok(result);
+	}
 	
 	
 }
--- src/main/java/com/exemple/demo/repositories/UserRepository.java
+++ src/main/java/com/exemple/demo/repositories/UserRepository.java
@@ -1,11 +1,27 @@
 package com.exemple.demo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.exemple.demo.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT obj FROM User obj " + "WHERE obj.salary >= :minSalary " + "AND obj.salary <= :maxSalary")
+	Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+
+	@Query("SELECT obj FROM User obj " + "WHERE LOWER(obj.name) like LOWER(CONCAT('%',:name,'%'))")
+	Page<User> searchByName(String name, Pageable pageable);
+
+	/*
+	 * usando metodo do próprio framework para encontrar o mesmo passo 
+	 * dos dois métodos anteriormente digitado
+	 */
 	
-	
+	Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+
+	Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
 }
